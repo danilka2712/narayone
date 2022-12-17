49fd2dc6f06c63a1dc80157bf482dffb312192f7
unknown <danilka2712@yandex.ru>
2022-12-17 16:06:17 +0600

    first commit

--- src/lib/components/InputAddress.svelte
+++ src/lib/components/InputAddress.svelte
@@ -1,47 +1,34 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import maplibregl from 'maplibre-gl';
-	import 'maplibre-gl/dist/maplibre-gl.css';
-	import DistanceMeasurementMapLibreGlDirections, {
-		config
-	} from '$lib/map/distance-measurement-directions';
-	let directions: DistanceMeasurementMapLibreGlDirections | undefined = undefined;
-	let totalDistance = 0;
-	onMount(() => {
-		const map = new maplibregl.Map({
-			container: 'map',
-			style:
-				'https://api.maptiler.com/maps/f0650ebb-77aa-4dca-bef9-006920409ea9/style.json?key=EfH47Bb8jzv9Pl57bst7',
-			center: [74.1197632, 54.6974034],
-			zoom: 11
+
+	$: addresse = '';
+	async function sumbit() {
+		let map = new maplibregl.Map({
+			container: 'map', // container id
+			style: 'https://api.maptiler.com/maps/f0650ebb-77aa-4dca-bef9-006920409ea9/style.json?key=EfH47Bb8jzv9Pl57bst7', // style URL
+			center: [74.3342, 54.534543], // starting position [lng, lat]
+			zoom: 12 // starting zoom
 		});
-		map.on('load', () => {
-			directions = new DistanceMeasurementMapLibreGlDirections(map, config);
-			directions.on('fetchroutesend', (ev) => {
-				totalDistance = ev.data?.routes[0].distance as number;
-			});
-			directions.on('removewaypoint', () => {
-				if (directions.waypoints.length < 2) {
-					totalDistance = 0;
-				}
-			});
-			directions.interactive = true;
-		});
-	});
+
+		const response = await fetch(
+			`https://nominatim.openstreetmap.org/search?q=${
+				addresse + ' ' + 'Омск'
+			}&countrycodes=ru&format=geojson&polygon_geojson=1&addressdetails=1`
+		);
+		const data = await response.json();
+	}
+
+	let item = '';
 </script>
 
-<p>
-	Total Route Distance:
-	{#if totalDistance}
-		<strong>{totalDistance}m</strong>
-	{:else}
-		<em>0</em>
-	{/if}
-</p>
+{item}
+
 <span class="text-[#8e8e8e]  text-sm">Укажите маршрут</span>
 <div class=" relative">
 	<form action="">
 		<input
+			bind:value={addresse}
+			on:click={sumbit}
 			placeholder="Лукашевича 25"
 			class="p-4 border-[#e8e8e8]/75 w-full font-sans focus:border-[#5BC43A]  focus:outline-none border py-4 rounded-2xl"
 			type="text"
