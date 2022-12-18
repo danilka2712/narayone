ad2a851defb4d3ce11e2c7edb20651f7d57feddb
unknown <danilka2712@yandex.ru>
2022-12-18 12:43:35 +0600

    first commit

--- src/lib/components/InputAddress.svelte
+++ src/lib/components/InputAddress.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-	$: coordinate = [];
-	$: coordinate1 = [];
-	let items = [];
-	$: address = '';
-	$: address1 = '';
-	let number = 0;
-	let hidden = false;
-	$: hidden === true ? sumbit() : number = 0
-	async function sumbit() {
-		number += 1;
+	import { MercatorCoordinate } from 'mapbox-gl';
+
+	let whereCoordinate;
+	let toCoordinate;
+
+	const mapOpen = (num) => {
 		setTimeout(() => {
-			if (number === 1) {
+			if (num === 1) {
 				let map = new maplibregl.Map({
 					container: 'map', // container id
 					style:
@@ -19,23 +15,12 @@
 					zoom: 12 // starting zoom
 				});
 
-				let marker = new maplibregl.Marker({ color: '#5BC43A' }).setLngLat([0, 0]).addTo(map);
-
-				map.on('dragstart', function (e) {
-					address = 'Определяем адрес';
-				});
-				map.on('dragend', function (e) {
-					fetch(
-						`https://nominatim.openstreetmap.org/reverse?format=json&lat=${coordinate[1]}&lon=${coordinate[0]}&zoom=18&addressdetails=1`
-					)
-						.then((data) => data.json())
-						.then((addresse) => (address = addresse.address.road));
-				});
+				let marker = new maplibregl.Marker().setLngLat([0, 0]).addTo(map);
 				map.on('move', function (e) {
-					let coordinates = marker.setLngLat(map.getCenter());
-					coordinate = coordinates._lngLat.toArray();
+					let center = marker.setLngLat(map.getCenter());
+					whereCoordinate = center.getLngLat().toArray();
 				});
-			} else if (number === 2) {
+			} else if (num === 2) {
 				let map = new maplibregl.Map({
 					container: 'map', // container id
 					style:
@@ -43,25 +28,13 @@
 					center: [74.3342, 55.534543], // starting position [lng, lat]
 					zoom: 12 // starting zoom
 				});
-
-				let marker = new maplibregl.Marker({ color: '#5BC43A' }).setLngLat([0, 0]).addTo(map);
-
-				map.on('dragstart', function (e) {
-					address = 'Определяем адрес';
-				});
-				map.on('dragend', function (e) {
-					fetch(
-						`https://nominatim.openstreetmap.org/reverse?format=json&lat=${coordinate[1]}&lon=${coordinate[0]}&zoom=18&addressdetails=1`
-					)
-						.then((data) => data.json())
-						.then((addresse) => (address = addresse.address.road));
-				});
+				new maplibregl.Marker().setLngLat(whereCoordinate).addTo(map);
+				let marker = new maplibregl.Marker().setLngLat([0, 0]).addTo(map);
 				map.on('move', function (e) {
-					let coordinates = marker.setLngLat(map.getCenter());
-					coordinate1 = coordinates._lngLat.toArray();
+					let center = marker.setLngLat(map.getCenter());
+					toCoordinate = center.getLngLat().toArray();
 				});
-				new maplibregl.Marker().setLngLat(coordinate).addTo(map);
-			} else if (number >= 2) {
+			} else if (num === 3) {
 				let map = new maplibregl.Map({
 					container: 'map', // container id
 					style:
@@ -69,12 +42,10 @@
 					center: [74.3342, 55.534543], // starting position [lng, lat]
 					zoom: 12 // starting zoom
 				});
-
-				new maplibregl.Marker({ color: '#5BC43A' }).setLngLat(coordinate).addTo(map);
-
-				new maplibregl.Marker({ color: '#5BC43A' }).setLngLat(coordinate1).addTo(map);
+				new maplibregl.Marker().setLngLat(whereCoordinate).addTo(map);
+				new maplibregl.Marker().setLngLat(toCoordinate).addTo(map);
 				fetch(
-					`https://api.mapbox.com/directions/v5/mapbox/driving/${coordinate};${coordinate1}?annotations=maxspeed&overview=full&geometries=geojson&access_token=pk.eyJ1IjoiZGFuaWxrYTI3MTIiLCJhIjoiY2xiamFndWc2MDJoazNwcXZnaXZoNm9hYSJ9.lAMLaj7C67amMgE1yWU_WA`
+					`https://api.mapbox.com/directions/v5/mapbox/driving/${whereCoordinate};${toCoordinate}?annotations=maxspeed&overview=full&geometries=geojson&access_token=pk.eyJ1IjoiZGFuaWxrYTI3MTIiLCJhIjoiY2xiamFndWc2MDJoazNwcXZnaXZoNm9hYSJ9.lAMLaj7C67amMgE1yWU_WA`
 				)
 					.then((data) => data.json())
 					.then((addresse) => {
@@ -106,20 +77,26 @@
 						}
 					});
 				});
-				number = 0
-			
 			}
-		}, 200);
+		}, 1000);
+	};
+	let hidden = false;
+	function activeMap() {
+		hidden = !hidden;
+		if (hidden === true) {
+			mapOpen(1);
+		}
 	}
-
-	let item = '';
+	let items;
 </script>
+
+<button on:click={() => mapOpen(2)}>Добавить</button>
+<button on:click={() => mapOpen(3)}>Добавить2</button>
 
 <span class="text-[#8e8e8e]  text-sm">Укажите маршрут</span>
 <div class=" relative mb-4">
 	<form action=" " class=" relative">
 		<input
-			bind:value={address}
 			placeholder="Лукашевича 25"
 			class="p-4 border-[#e8e8e8]/75 w-full font-sans focus:border-[#5BC43A]  focus:outline-none border py-4 rounded-2xl"
 			type="text"
@@ -127,12 +104,11 @@
 			id=""
 			autocomplete="address-line1"
 		/>
-		<span on:click={() => hidden = !hidden} class=" absolute p-4 right-0 cursor-pointer">Карта</span>
+		<span on:click={() => activeMap()} class=" absolute p-4 right-0 cursor-pointer">Карта</span>
 	</form>
 </div>
 <div class="mb-4">
 	<input
-		bind:value={address1}
 		placeholder="Мира 31"
 		class="p-4 border-[#e8e8e8]/75 w-full font-sans focus:border-[#5BC43A]  focus:outline-none border py-4 rounded-2xl"
 		type="text"
@@ -140,20 +116,12 @@
 		id=""
 	/>
 </div>
-{number}
 {#if hidden}
 	<div id="map" class="bg-[#EEEEEE] relative h-[50vh]">
-		{#if number >= 1}
 		<div class=" absolute bottom-2 z-10 w-full px-4">
-			<button class=" text-white w-full  bg-[#5BC43A] rounded-xl p-4 z-10" on:click={() => sumbit()}
+			<button class=" text-white w-full  bg-[#5BC43A] rounded-xl p-4 z-10"
 				>Выбрать точку отправления</button
 			>
 		</div>
-		{:else if number === 0}
-			<button
-				class=" absolute bottom-3 bg-[#5BC43A] rounded-xl p-4 z-10"
-				on:click={() => sumbit()}>Очистить</button
-			>
-		{/if}
 	</div>
 {/if}
