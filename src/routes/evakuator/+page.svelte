4af689bedb2fb9d544cb65feef66c3bacc3f0f99
unknown <danilka2712@yandex.ru>
2022-12-21 21:09:58 +0600

    first commit

--- src/routes/evakuator/+page.svelte
+++ src/routes/evakuator/+page.svelte
@@ -182,7 +182,7 @@
 			<p class=" text-xl font-semibold font-sans">{selected === 'BMW' ? '1990' : '2390'}₽</p>
 		</div>
 		<a href="/">
-			<button class=" bg-[#8093FF] p-3 mt-2 rounded-2xl w-full   py-4   font-semibold text-white"
+			<button class=" bg-[#5BC43A] p-3 mt-2 rounded-2xl w-full   py-4   font-semibold text-white"
 				>Оставить заявку</button
 			>
 		</a>
