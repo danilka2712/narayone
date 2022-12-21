9d9aa2425787b233c0713afd86307d5ffbeaf96c
unknown <danilka2712@yandex.ru>
2022-12-21 23:34:40 +0600

    first commit

--- src/routes/evakuator/+page.svelte
+++ src/routes/evakuator/+page.svelte
@@ -181,7 +181,7 @@
 			<span class="my-4 text-[#8e8e8e] text">Стоимость:</span>
 			<p class=" text-xl font-semibold font-sans">{selected === 'BMW' ? '1990' : '2390'}₽</p>
 		</div>
-		<a href="/">
+		<a href="/order/2">
 			<button class=" bg-[#5BC43A] p-3 mt-2 rounded-2xl w-full   py-4   font-semibold text-white"
 				>Оставить заявку</button
 			>
--- src/routes/order/[id]/+page.svelte
+++ src/routes/order/[id]/+page.svelte
@@ -8,8 +8,17 @@
 	$: seconds = Math.floor(original - minutes * 60);
 </script>
 
-<div class="px-5 mb-12">
-	<div class="  rounded-xl py-8 my-4 p-4">
-		<p class=" text-center   text-8xl">{minutes}:{seconds < 10 ? '0' : ''}{seconds}</p>
+<div class="h-[85vh] flex flex-col justify-between   px-5">
+	<div class=" items-end  rounded-xl flex justify-between mt-5">
+		<p class="font-medium animate-pulse text-[#8e8e8e]">Мы подбираем для вас эвакуатор</p>
 	</div>
+	<div class=" flex justify-center">
+		<img src="/car3.svg" class="w-44 animate-pulse "  alt="">
+	</div>
+	<a class="bottom-5  " href="/">
+		<button
+			class="font-sans bg-[#0d1722] p-3 mt-2 rounded-2xl w-full   py-4   font-semibold text-white"
+			>Поиск займет примерно: {minutes}:{seconds < 10 ? '0' : ''}{seconds}</button
+		>
+	</a>
 </div>
