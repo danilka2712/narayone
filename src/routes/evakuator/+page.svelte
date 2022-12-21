119540c6cbceedcc71a6678d10dc1a8f7f3053ee
unknown <danilka2712@yandex.ru>
2022-12-21 17:02:12 +0600

    first commit

--- src/routes/evakuator/+page.svelte
+++ src/routes/evakuator/+page.svelte
@@ -90,10 +90,27 @@
 				{/each}
 			</select>
 		</div>
-		<div class="mt-4">
-			<button on:click={() => hiddenPogruz = !hiddenPogruz} class="">Сложность погрузки</button>
+		<div class="mt-5 flex justify-end">
+			<div class="flex items-center gap-2">
+				<button on:click={() => (hiddenPogruz = !hiddenPogruz)} class="">Сложность погрузки</button>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="100%"
+					height="100%"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="feather mt-1 feather-chevron-up rotate-180 w-4 h-4"
+				>
+					<polyline points="18 15 12 9 6 15" />
+				</svg>
+			</div>
+
 			{#if hiddenPogruz}
-				<div class="bg-white h-44 overflow-scroll gap-4 mt-4 rounded-xl absolute p-5 flex flex-col">
+				<div class="bg-white h-44 overflow-scroll gap-4 mt-8 rounded-xl absolute p-5 flex flex-col">
 					<label class="flex gap-4 items-center">
 						<input type="checkbox" />
 						Вытащить с кувета
