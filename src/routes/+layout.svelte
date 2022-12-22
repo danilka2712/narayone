ee510632ca6fa2f978da0c24f91d7ecf1c63be6a
unknown <danilka2712@yandex.ru>
2022-12-22 22:03:42 +0600

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,14 +5,14 @@
 
 <nav class="px-5 py-6 flex justify-between items-center">
 	<a href="/"><img class=" w-32" src="/logo.svg" alt="" /></a>
-	<a href="/register"><img class="w-4 rounded-full" src="/user.svg" alt="" /></a>
+	<a href="/register"><img class="w-5" src="/Profile.svg" alt="" /></a>
 </nav>
 
 <a
 	class="fixed box bottom-7 left-5 bg-[#F3F3F4] flex items-center justify-center border rounded-xl p-3 w-14 h-14 z-10"
 	href="/"
 >
-	<img class="" src="/home.svg" alt="" /></a
+	<img class="" src="/Profile.svg" alt="" /></a
 >
 
 <slot />
