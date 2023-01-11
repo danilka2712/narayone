105eeb3addc3ec2159d1c7262523b4331eb62520
unknown <danilka2712@yandex.ru>
2023-01-11 12:09:44 +0600

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,17 +13,27 @@
 		menuHiiden = !menuHiiden;
 	}
 </script>
+{#if menuHiiden}
+<div  class=" absolute bg-black/5 h-full w-full z-30  ">
+	<ul class="flex bg-white absolute top-14 right-5 rounded p-6 w-1/2  flex-col gap-4">
+		<li><a href="/dashboard">Личный кабинет</a></li>
+		<li><a href="/dashboard/orders">Новые заявки</a></li>
+		<li><a href="/dashboard/myorder">Принятые заявки</a></li>
+	</ul>
+</div>
+{/if}
 
-
-<nav class="container flex items-center justify-between  mx-auto px-5 mb-4  h-[7vh] py-8">
+<nav use:clickOutside on:outclick={() => (menuHiiden = false)} class="container relative z-40 flex items-center justify-between  mx-auto px-5 mb-4  h-[7vh] py-8">
 	<a href="/">
 		<img width="101px" src="/logo.svg" alt="" />
 	</a>
 	<div class="flex gap-8">
 		<img width="26px" src="/Messages.svg" alt="" />
-		<img width="26px" src="/User.svg" alt="" />
+		<button on:click={menuHidden}>
+			<img width="26px" src="/User.svg" alt="" />
+
+		</button>
 	</div>
 </nav>
-
 
 <slot />
